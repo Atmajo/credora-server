3c8cff1680a7de3cac3e4e1862e6e9d00b2e828f
atmajo chowdhury <atmajoc@gmail.com>
2025-08-23 22:55:35 +0530

    feat: update verifyAccountTemplate for improved branding and layout

--- src/templates/verifyAccount.ts
+++ src/templates/verifyAccount.ts
@@ -1,45 +1,66 @@
-export const verifyAccountTemplate = (
-  link: string
-) => `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html dir="ltr" lang="en">
-  <head>
+export const verifyAccountTemplate = (link: string) => `
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html
+  data-source-file="../emails/notifications/yelp-recent-login.tsx"
+  data-source-line="41"
+  dir="ltr"
+  lang="en">
+  <head
+    data-source-file="../emails/notifications/yelp-recent-login.tsx"
+    data-source-line="42">
     <link
       rel="preload"
       as="image"
-      href="https://react-email-demo-macyus16n-resend.vercel.app/static/aws-logo.png" />
+      href="https://react-email-demo-nw7ulvj51-resend.vercel.app/static/yelp-logo.png" />
+    <link
+      rel="preload"
+      as="image"
+      href="https://react-email-demo-nw7ulvj51-resend.vercel.app/static/yelp-header.png" />
+    <link
+      rel="preload"
+      as="image"
+      href="https://react-email-demo-nw7ulvj51-resend.vercel.app/static/yelp-footer.png" />
     <meta content="text/html; charset=UTF-8" http-equiv="Content-Type" />
     <meta name="x-apple-disable-message-reformatting" />
     <!--$-->
   </head>
-  <body style="background-color:#fff;color:#212121">
-    <div
-      style="display:none;overflow:hidden;line-height:1px;opacity:0;max-height:0;max-width:0"
-      data-skip-in-text="true">
-      AWS Email Verification
-      <div>
-      </div>
-    </div>
+  <body
+    data-source-file="../emails/notifications/yelp-recent-login.tsx"
+    data-source-line="43"
+    style="background-color:#fff">
     <table
-      align="center"
-      width="100%"
       border="0"
+      width="100%"
       cellpadding="0"
       cellspacing="0"
       role="presentation"
-      style="max-width:37.5em;padding:20px;margin:0 auto;background-color:#eee">
+      align="center">
       <tbody>
-        <tr style="width:100%">
-          <td>
+        <tr>
+          <td
+            style='background-color:#fff;font-family:-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif'>
+            <div
+              style="display:none;overflow:hidden;line-height:1px;opacity:0;max-height:0;max-width:0"
+              data-skip-in-text="true"
+              data-source-file="../emails/notifications/yelp-recent-login.tsx"
+              data-source-line="44">
+              Yelp recent login
+              <div>
+                 ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿
+              </div>
+            </div>
             <table
               align="center"
               width="100%"
+              data-source-file="../emails/notifications/yelp-recent-login.tsx"
+              data-source-line="45"
               border="0"
               cellpadding="0"
               cellspacing="0"
               role="presentation"
-              style="background-color:#fff">
+              style="max-width:37.5em">
               <tbody>
-                <tr>
+                <tr style="width:100%">
                   <td>
                     <table
                       align="center"
@@ -48,43 +69,12 @@ export const verifyAccountTemplate = (
                       cellpadding="0"
                       cellspacing="0"
                       role="presentation"
-                      style="background-color:#252f3d;display:flex;padding:20px 0;align-items:center;justify-content:center">
+                      data-source-file="../emails/notifications/yelp-recent-login.tsx"
+                      data-source-line="50"
+                      style="border:1px solid rgb(0,0,0, 0.1);border-radius:3px;overflow:hidden">
                       <tbody>
                         <tr>
                           <td>
-                            <img
-                              alt="AWS&#x27;s Logo"
-                              height="45"
-                              src="https://react-email-demo-macyus16n-resend.vercel.app/static/aws-logo.png"
-                              style="display:block;outline:none;border:none;text-decoration:none"
-                              width="75" />
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                    <table
-                      align="center"
-                      width="100%"
-                      border="0"
-                      cellpadding="0"
-                      cellspacing="0"
-                      role="presentation"
-                      style="padding:25px 35px">
-                      <tbody>
-                        <tr>
-                          <td>
-                            <h1
-                              style="color:#333;font-family:-apple-system, BlinkMacSystemFont, &#x27;Segoe UI&#x27;, &#x27;Roboto&#x27;, &#x27;Oxygen&#x27;, &#x27;Ubuntu&#x27;, &#x27;Cantarell&#x27;, &#x27;Fira Sans&#x27;, &#x27;Droid Sans&#x27;, &#x27;Helvetica Neue&#x27;, sans-serif;font-size:20px;font-weight:bold;margin-bottom:15px">
-                              Verify your email address
-                            </h1>
-                            <p
-                              style="font-size:14px;line-height:24px;color:#333;font-family:-apple-system, BlinkMacSystemFont, &#x27;Segoe UI&#x27;, &#x27;Roboto&#x27;, &#x27;Oxygen&#x27;, &#x27;Ubuntu&#x27;, &#x27;Cantarell&#x27;, &#x27;Fira Sans&#x27;, &#x27;Droid Sans&#x27;, &#x27;Helvetica Neue&#x27;, sans-serif;margin:24px 0;margin-bottom:14px;margin-top:24px;margin-right:0;margin-left:0">
-                              Thanks for starting the new AWS account creation
-                              process. We want to make sure it&#x27;s really
-                              you. Please enter the following verification code
-                              when prompted. If you don&#x27;t want to create an
-                              account, you can ignore this message.
-                            </p>
                             <table
                               align="center"
                               width="100%"
@@ -92,25 +82,84 @@ export const verifyAccountTemplate = (
                               cellpadding="0"
                               cellspacing="0"
                               role="presentation"
-                              style="display:flex;align-items:center;justify-content:center">
-                              <tbody>
-                                <tr>
-                                  <td>
+                              data-source-file="../emails/notifications/yelp-recent-login.tsx"
+                              data-source-line="51">
+                              <tbody style="width:100%">
+                                <tr style="width:100%">
+                                  <img
+                                    data-source-file="../emails/notifications/yelp-recent-login.tsx"
+                                    data-source-line="52"
+                                    alt="Yelp header illustration"
+                                    src="https://2zek8lj0x9.ufs.sh/f/x6c1qp2r3WfDrmtHSIJenvO8imMyT31RQSjlK6hwFq0EXWsC"
+                                    style="display:block;outline:none;border:none;text-decoration:none;max-width:100%"
+                                    width="620" />
+                                </tr>
+                              </tbody>
+                            </table>
+                            <table
+                              align="center"
+                              width="100%"
+                              border="0"
+                              cellpadding="0"
+                              cellspacing="0"
+                              role="presentation"
+                              data-source-file="../emails/notifications/yelp-recent-login.tsx"
+                              data-source-line="60"
+                              style="padding:20px;padding-bottom:0">
+                              <tbody style="width:100%">
+                                <tr style="width:100%">
+                                  <td
+                                    data-source-file="../emails/notifications/yelp-recent-login.tsx"
+                                    data-source-line="61"
+                                    <h2
+                                      data-source-file="../emails/notifications/yelp-recent-login.tsx"
+                                      data-source-line="71"
+                                      style="font-size:26px;font-weight:bold;text-align:center">
+                                      Well come to Credora.
+                                    </h2>
                                     <p
-                                      style="font-size:14px;line-height:24px;color:#333;font-family:-apple-system, BlinkMacSystemFont, &#x27;Segoe UI&#x27;, &#x27;Roboto&#x27;, &#x27;Oxygen&#x27;, &#x27;Ubuntu&#x27;, &#x27;Cantarell&#x27;, &#x27;Fira Sans&#x27;, &#x27;Droid Sans&#x27;, &#x27;Helvetica Neue&#x27;, sans-serif;margin:0;font-weight:bold;text-align:center;margin-top:0;margin-bottom:0;margin-left:0;margin-right:0">
-                                      Verification code
+                                      data-source-file="../emails/notifications/yelp-recent-login.tsx"
+                                      data-source-line="82"
+                                      style="font-size:16px;line-height:24px;margin-top:16px;margin-bottom:16px">
+                                      Please very your account with Credora
                                     </p>
+                                  </td>
+                                </tr>
+                              </tbody>
+                            </table>
+                            <table
+                              align="center"
+                              width="100%"
+                              border="0"
+                              cellpadding="0"
+                              cellspacing="0"
+                              role="presentation"
+                              data-source-file="../emails/notifications/yelp-recent-login.tsx"
+                              data-source-line="114"
+                              style="padding:20px;padding-top:0">
+                              <tbody style="width:100%">
+                                <tr style="width:100%">
+                                  <td
+                                    colspan="2"
+                                    data-source-file="../emails/notifications/yelp-recent-login.tsx"
+                                    data-source-line="115"
+                                    data-id="__react-email-column"
+                                    style="text-align:center">
                                     <a
-                                      style="font-size:36px;line-height:24px;color:#333;font-family:-apple-system, BlinkMacSystemFont, &#x27;Segoe UI&#x27;, &#x27;Roboto&#x27;, &#x27;Oxygen&#x27;, &#x27;Ubuntu&#x27;, &#x27;Cantarell&#x27;, &#x27;Fira Sans&#x27;, &#x27;Droid Sans&#x27;, &#x27;Helvetica Neue&#x27;, sans-serif;margin:10px 0;font-weight:bold;text-align:center;margin-top:10px;margin-right:0;margin-bottom:10px;margin-left:0"
-                                      href="${link}"
+                                      href=${link}
+                                      data-source-file="../emails/notifications/yelp-recent-login.tsx"
+                                      data-source-line="116"
+                                      style="line-height:100%;text-decoration:none;display:inline-block;max-width:100%;mso-padding-alt:0px;background-color:#000;border-radius:3px;color:#FFF;font-weight:bold;border:1px solid rgb(0,0,0,0);cursor:pointer;padding:12px 30px;padding-top:12px;padding-right:30px;padding-bottom:12px;padding-left:30px"
                                       target="_blank"
+                                      ><span
+                                        ><!--[if mso]><i style="mso-font-width:500%;mso-text-raise:18" hidden>&#8202;&#8202;&#8202;</i><![endif]--></span
+                                      ><span
+                                        style="max-width:100%;display:inline-block;line-height:120%;mso-padding-alt:0px;mso-text-raise:9px"
+                                        >Learn More</span
+                                      ><span
+                                        ><!--[if mso]><i style="mso-font-width:500%" hidden>&#8202;&#8202;&#8202;&#8203;</i><![endif]--></span
+                                      ></a
                                     >
-                                      Reset Password
-                                    </a>
-                                    <p
-                                      style="font-size:14px;line-height:24px;color:#333;font-family:-apple-system, BlinkMacSystemFont, &#x27;Segoe UI&#x27;, &#x27;Roboto&#x27;, &#x27;Oxygen&#x27;, &#x27;Ubuntu&#x27;, &#x27;Cantarell&#x27;, &#x27;Fira Sans&#x27;, &#x27;Droid Sans&#x27;, &#x27;Helvetica Neue&#x27;, sans-serif;margin:0px;text-align:center;margin-top:0px;margin-bottom:0px;margin-left:0px;margin-right:0px">
-                                      (This code is valid for 10 minutes)
-                                    </p>
                                   </td>
                                 </tr>
                               </tbody>
@@ -119,8 +168,6 @@ export const verifyAccountTemplate = (
                         </tr>
                       </tbody>
                     </table>
-                    <hr
-                      style="width:100%;border:none;border-top:1px solid #eaeaea" />
                     <table
                       align="center"
                       width="100%"
@@ -128,16 +175,19 @@ export const verifyAccountTemplate = (
                       cellpadding="0"
                       cellspacing="0"
                       role="presentation"
-                      style="padding:25px 35px">
+                      data-source-file="../emails/notifications/yelp-recent-login.tsx"
+                      data-source-line="121"
+                      style="padding:45px 0 0 0">
                       <tbody>
                         <tr>
                           <td>
-                            <p
-                              style="font-size:14px;line-height:24px;color:#333;font-family:-apple-system, BlinkMacSystemFont, &#x27;Segoe UI&#x27;, &#x27;Roboto&#x27;, &#x27;Oxygen&#x27;, &#x27;Ubuntu&#x27;, &#x27;Cantarell&#x27;, &#x27;Fira Sans&#x27;, &#x27;Droid Sans&#x27;, &#x27;Helvetica Neue&#x27;, sans-serif;margin:0px;margin-top:0px;margin-bottom:0px;margin-left:0px;margin-right:0px">
-                              Amazon Web Services will never email you and ask
-                              you to disclose or verify your password, credit
-                              card, or banking account number.
-                            </p>
+                            <img
+                              data-source-file="../emails/notifications/yelp-recent-login.tsx"
+                              data-source-line="122"
+                              alt="Yelp footer decoration"
+                              src="https://react-email-demo-nw7ulvj51-resend.vercel.app/static/yelp-footer.png"
+                              style="display:block;outline:none;border:none;text-decoration:none;max-width:100%"
+                              width="620" />
                           </td>
                         </tr>
                       </tbody>
@@ -152,5 +202,4 @@ export const verifyAccountTemplate = (
     </table>
     <!--/$-->
   </body>
-</html>
-`;
+</html>`;
